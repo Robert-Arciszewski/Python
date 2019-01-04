59313e66c3ac1ed9462033b99039f290636b1298
zort <robert.arciszewski8@gmail.com>
2019-01-04 13:23:15 +0100

    More hash types added

--- Hashing/hash.py
+++ Hashing/hash.py
@@ -5,7 +5,7 @@ from prompt_toolkit import prompt
 def hashing():
     h = hashtext()
     t = input("Text to convert: ")
-    k = WordCompleter(['md5', 'sha512', 'blake2b', 'sha384','shake_128','sha3_384','sha512','sha3_512','shake_256','sha224','sha1','sha3_224','blake2s','sha3_256'])
+    k = WordCompleter(['blake2b', 'sha3_384', 'md5', 'MD5-SHA1', 'BLAKE2s256', 'sha512', 'sha3_256', 'RIPEMD160', 'sha384', 'SHA384', 'whirlpool', 'shake_128', 'SHA1', 'SHA224', 'MD5', 'sha256', 'blake2b512', 'SHA512', 'sha224', 'shake_256', 'blake2s', 'sha3_224', 'md4', 'sha1', 'blake2s256', 'SHA256', 'sha3_512', 'BLAKE2b512', 'md5-sha1', 'MD4', 'ripemd160'])
     k = prompt('Hash type: ', completer=k)
     kd = str(k)
     print(t +":"+ h.hashingtext(t,kd))
--- Hashing/hash_class.py
+++ Hashing/hash_class.py
@@ -3,6 +3,7 @@ import hashlib
 class hashtext:
     def hashingtext(self, text, hash_type):
         #print(hashlib.algorithms_guaranteed)
+        #print(hashlib.algorithms_available)
         text = text.encode("utf-8")
         hash = hashlib.new(hash_type)
         hash.update(text)
